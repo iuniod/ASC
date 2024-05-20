59d30c7d4210eceb8c675a6c52c39818651457c4
Iustina-Andreea Caramida <iustina.caramida@gmail.com>
2024-05-20 12:32:17 +0300

    optimize opt solution

--- homework/3-optimizari/src/solver_opt.c
+++ homework/3-optimizari/src/solver_opt.c
@@ -9,25 +9,32 @@
  */
 double* my_solver(int N, double *A, double* B) {
 	register int N2 = N * N;
-	int i, j, k;
+	int i, k;
 	double *C = (double*)malloc(N2 * sizeof(double));
 	double *D = (double*)malloc(N2 * sizeof(double));
 
 	// Multiply A^T with B and store the result in C
-    for (register double *original_b = B; original_b < B + N; original_b ++) {
-        for (i = 0; i < N; i++) {
+	register double *ptr_original_c = C;
+	for (register double *original_a = A; original_a < A + N; original_a++) {
+		register double *ptr_c = ptr_original_c;
+    	for (i = 0; i < N; i++) {
+			register double *pa = original_a;
+			register double *pb = B + i;
             register double suma = 0;
-            register double *pa = A + i;
-            register double *pb = original_b;
-            for (k = 0; k <= i; k++, pa += N, pb += N) {
+            for (k = 0; k <= (original_a - A); k++, pa += N, pb += N) {
                 suma += *pa * *pb;
             }
-            C[i * N + (original_b - B)] = suma;
+            *ptr_c = suma;
+			ptr_c++;
         }
+
+		ptr_original_c += N;
     }
 
 	// Multiply B with A and store the result in D
+	register double *ptr_original_d = D;
 	for (register double *original_b = B; original_b < B + N2; original_b+=N) {
+		register double *ptr_d = ptr_original_d;
 		for (i = 0; i < N; i++) {
 			register double suma = 0;
 			register double *pb = original_b;
@@ -35,28 +42,35 @@ double* my_solver(int N, double *A, double* B) {
 			for (k = 0; k <= i; k++, pb++, pa += N) {
 				suma += *pb * *pa;
 			}
-			D[i + (original_b - B)] = suma;
+			*ptr_d = suma;
+			ptr_d++;
 		}
+
+		ptr_original_d += N;
 	}
 
 	// Add C and D and store the result in C
-	double *pc = C;
 	register double *pd = D;
-	for (i = 0; i < N2; i++, pc++, pd++) {
+	for (double *pc = C; (pc - C) < N2; pc++, pd++) {
 		*pc = *pc + *pd;
 	}
 
 	// Multiply C with B^T and store the result in D
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
+	ptr_original_d = D;
+    for (register double *original_c = C; original_c < C + N2; original_c += N) {
+		register double *ptr_d = ptr_original_d;
+        for (i = 0; i < N2; i += N) {
             register double suma = 0;
-            register double *pc = C + i * N;
-            register double *pb = B + j * N;
-            for (k = 0; k < N; k++, pc++, pb++) {
+            register double *pc = original_c;
+            register double *pb = B + i;
+            for (; (pc - original_c) < N; pc++, pb++) {
                 suma += *pc * *pb;
             }
-            D[i * N + j] = suma;
+			*ptr_d = suma;
+			ptr_d++;
         }
+
+		ptr_original_d += N;
     }
 
 	free(C);
